8b95e99a90844c4a1656f08f938b4e36c5cd09f8
Shawn Peters Briones <shawnpeters93@yahoo.com>
2022-03-29 14:44:02 -0600

    Bug fix for admin init

--- classes/Admin.php
+++ classes/Admin.php
@@ -3,7 +3,7 @@
 namespace Classes\Admin;
 
 class Setup {
-	
+
 	public function __construct() {
 		if ( is_admin() ) {
 			add_filter( 'plugin_action_links_' . basename( dirname( TOTALLYCRITICALCSS_PLUGIN_FILE ) ) . '/totallycriticalcss.php', array( $this, 'add_settings_link' ) );
@@ -16,7 +16,7 @@ class Setup {
 			add_action( 'wp_ajax_totallycriticalcss_delete_custum_route', array( $this, 'delete_custum_route' ) );
 		}
 	}
-	
+
 	/**
 	* Add settings link on plugin page
 	*/
@@ -36,8 +36,10 @@ class Setup {
 	* Register and enqueue admin stylesheet & scripts
 	*/
 	public function admin_init() {
+		$current_page = admin_url( 'options-general.php?page=' . $_GET[ 'page' ] );
+
 		// only enqueue these things on the settings page
-		if ( wc_get_current_admin_url() == $this->get_admin_url() ) {
+		if ( $current_page == $this->get_admin_url() ) {
 			wp_register_style( 'totallycriticalcss_plugin_stylesheet', plugin_dir_url( TOTALLYCRITICALCSS_PLUGIN_FILE ) . 'admin/css/admin.css' );
 			wp_enqueue_style( 'totallycriticalcss_plugin_stylesheet' );
 			wp_register_script( 'totallycriticalcss_script', plugin_dir_url( TOTALLYCRITICALCSS_PLUGIN_FILE ) . 'admin/js/admin.js', array( 'jquery' ) );
@@ -76,79 +78,79 @@ class Setup {
 	* Update Form Data when submitted
 	*/
 	public function save_admin_page() {
-		
+
 		$literals = [ 'api_key', 'selected_styles', 'selected_cpt' ];
 		$bools = [ 'simplemode', 'show_metaboxes', 'always_immediate', 'adminmode' ];
-		
+
 		foreach ( $literals as $key ) {
 			if ( isset( $_POST[ $key ] ) ) {
 				update_option( 'totallycriticalcss_' . $key, $_POST[ $key ] );
 			}
 		}
-		
+
 		foreach ( $bools as $key ) {
 			if ( isset( $_POST[ $key ] ) ) {
 				update_option( 'totallycriticalcss_' . $key, $_POST[ $key ] == 'true' );
 			}
 		}
-		
+
 		\TotallyCriticalCSS::clear_tccss_data();
-		
+
 	}
-	
+
 	public function add_custum_dequeue() {
-		
+
 		$form_handle = $_POST[ 'form_handle' ];
 		$form_url = $_POST[ 'form_url' ];
-		
+
 		$custom_dequeue = get_option( 'totallycriticalcss_custom_dequeue' ) === false ? [] : get_option( 'totallycriticalcss_custom_dequeue' );
 		$custom_dequeue[$form_handle] = $form_url;
-		
+
 		update_option( 'totallycriticalcss_custom_dequeue', $custom_dequeue );
 		\TotallyCriticalCSS::clear_tccss_data();
-		
+
 		wp_send_json_success( $custom_dequeue );
-		
+
 	}
-	
+
 	public function delete_custum_dequeue() {
-		
+
 		$form_handle = $_POST[ 'form_handle' ];
-		
+
 		$custom_dequeue = get_option( 'totallycriticalcss_custom_dequeue' ) === false ? [] : get_option( 'totallycriticalcss_custom_dequeue' );
 		unset( $custom_dequeue[$form_handle] );
-		
+
 		update_option( 'totallycriticalcss_custom_dequeue', $custom_dequeue );
 		\TotallyCriticalCSS::clear_tccss_data();
-		
+
 		wp_send_json_success( $custom_dequeue );
-		
+
 	}
-	
+
 	public function add_custum_route() {
-		
+
 		$form_url = $_POST[ 'form_url' ];
-		
+
 		$custom_routes = get_option( 'totallycriticalcss_custom_routes' ) === false ? [] : get_option( 'totallycriticalcss_custom_routes' );
 		$custom_routes[] = $form_url;
-		
+
 		update_option( 'totallycriticalcss_custom_routes', $custom_routes );
-		
+
 		wp_send_json_success( $custom_routes );
-		
+
 	}
-	
+
 	public function delete_custum_route() {
-		
+
 		$form_url = $_POST[ 'form_url' ];
-		
+
 		$custom_routes = get_option( 'totallycriticalcss_custom_routes' ) === false ? [] : get_option( 'totallycriticalcss_custom_routes' );
 		array_splice( $custom_routes, array_search( $form_url, $custom_routes ), 1) ;
-		
+
 		update_option( 'totallycriticalcss_custom_routes', $custom_routes );
-		
+
 		wp_send_json_success( $custom_routes );
-		
+
 	}
 
 }
